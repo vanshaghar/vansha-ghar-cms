b8d7f4f42d27fa44d358a6f2289d91e734378e05
Kishor U <blackkspydo@gmail.com>
2024-04-12 17:46:41 +0545

    Refactor Category collection configuration

--- src/collections/Category/index.ts
+++ src/collections/Category/index.ts
@@ -1,41 +1,43 @@
-import { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from "payload/types";
 
 export const Category: CollectionConfig = {
-  slug: 'category',
+  slug: "category",
   labels: {
-    singular: 'Category',
-    plural: 'Categories',
+    singular: "Category",
+    plural: "Categories"
+  },
+  admin: {
+    useAsTitle: "name"
   },
   access: {
     create: () => true,
     read: () => true,
     update: () => true,
-    delete: () => true,
+    delete: () => true
   },
   fields: [
     {
-      name: 'name',
-      label: 'Name',
-      type: 'text',
-      required: true,
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true
     },
     {
-      name: 'value',
-      label: 'Value',
-      type: 'text',
-      required: true,
+      name: "value",
+      label: "Value",
+      type: "text",
+      required: true
     },
     {
-      name: 'description',
-      label: 'Description',
-      type: 'textarea',
+      name: "description",
+      label: "Description",
+      type: "textarea"
     },
     {
-      name: 'parent',
-      label: 'Parent Category',
-      type: 'relationship',
-      relationTo: 'category',
+      name: "parent",
+      label: "Parent Category",
+      type: "relationship",
+      relationTo: "category"
     }
   ]
-
-}
+};
