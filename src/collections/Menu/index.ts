be05f562c170737a2dcdc7e1b4511095b8d7c334
Kishor U <blackkspydo@gmail.com>
2024-04-12 17:46:49 +0545

    Add Menu collection configuration

---
+++ src/collections/Menu/index.ts
@@ -0,0 +1,61 @@
+import { CollectionConfig } from "payload/types";
+
+export const Menu: CollectionConfig = {
+  slug: "menu",
+  labels: {
+    singular: "Menu",
+    plural: "Menus"
+  },
+  access: {
+    create: () => true,
+    read: () => true,
+    update: () => true,
+    delete: () => true
+  },
+  fields: [
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true
+    },
+    {
+      name: "description",
+      label: "Description",
+      type: "textarea"
+    },
+    {
+      name: "category",
+      label: "Category",
+      type: "relationship",
+      relationTo: "category",
+      admin: {
+        position: "sidebar"
+      }
+    },
+    {
+      name: "price",
+      label: "Price",
+      type: "number",
+      required: true
+    },
+    {
+      name: "image",
+      label: "Image",
+      type: "relationship",
+      relationTo: "media"
+    },
+    {
+      name: "foodType",
+      label: "Food Type",
+      type: "relationship",
+      relationTo: "food-type",
+      hasMany: true
+    },
+    {
+      name: "ingredients",
+      type: "textarea",
+      label: "Ingredients"
+    }
+  ]
+};
